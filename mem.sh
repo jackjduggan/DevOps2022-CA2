515f5b3a03dd2a388ed24779eda194d023efe8e8
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2022-12-05 16:53:20 +0000

    Add files via upload

---
+++ mem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+USEDMEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
+TCP_CONN=$(netstat -an | wc -l)
+TCP_CONN_PORT_80=$(netstat -an | grep 80 | wc -l)
+IO_WAIT=$(iostat | awk 'NR==4 {print $4}')
+
+aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY
+aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
+aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_80
+aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT
